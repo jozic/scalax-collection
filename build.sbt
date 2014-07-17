c8847f3d69ecfc471cdb3d5676ca0e47f4dde0e7
Eugene Platonov <jozic@live.com>
2014-07-17 00:07:12 -0400

    initial commit - add concurrent map and set factories

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "scalax-collection"
+
+version := "0.1.0-SNAPSHOT"
+
+organization := "com.daodecode"
+
+scalaVersion := "2.10.4"
+
+scalacOptions := Seq("-deprecation", "-unchecked", "-Xlint", "-Xfatal-warnings")
+
---
+++ src/main/scala/com/daodecode/scalax/collection/concurrent/package.scala
@@ -0,0 +1,29 @@
+package com.daodecode.scalax.collection
+
+import java.lang
+import java.util.Collections
+import java.util.concurrent.ConcurrentHashMap
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+
+package object concurrent {
+
+  type ConcurrentMap[K, V] = collection.concurrent.Map[K, V]
+
+  object ConcurrentMap {
+
+    /**
+     * Builds empty ConcurrentMap
+     */
+    def empty[K, V]: ConcurrentMap[K, V] = new ConcurrentHashMap[K, V]().asScala
+  }
+
+  object ConcurrentSet {
+    /**
+     * Builds empty set baked by ConcurrentMap
+     */
+    def empty[K]: mutable.Set[K] = Collections.newSetFromMap[K](new ConcurrentHashMap[K, lang.Boolean]()).asScala
+  }
+
+}
