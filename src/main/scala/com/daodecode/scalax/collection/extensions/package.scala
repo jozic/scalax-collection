73abea26a1aa4ee94ffcd8f4a3882e5eb07a25c5
Eugene Platonov <jozic@live.com>
2014-07-25 21:51:58 -0400

    add scaladoc to fold methods

--- src/main/scala/com/daodecode/scalax/collection/extensions/package.scala
+++ src/main/scala/com/daodecode/scalax/collection/extensions/package.scala
@@ -28,6 +28,17 @@ package object extensions {
 
   implicit class GenIterableLikeExtension[+A, +Repr <: GenIterableLike[A, Repr]](val iterableLike: GenIterableLike[A, Repr]) extends AnyVal {
 
+    /** Applies a binary operator to a start value and all elements of this collection while predicate `p` is satisfied,
+      * going left to right.
+      *
+      * @param   z    the start value.
+      * @param   p    the predicate used to test if fold should continue.
+      * @param   op   the binary operator.
+      * @tparam  B    the result type of the binary operator.
+      * @return  the result of inserting `op` between consecutive elements of this $coll,
+      *          going left to right with the start value `z` on the left.
+      */
+
     def foldLeftWhile[B](z: B)(p: B => Boolean)(op: (B, A) => B): B = {
       var result = z
       val it = iterableLike.iterator
@@ -38,6 +49,16 @@ package object extensions {
       result
     }
 
+    /** Applies a binary operator to all elements of this collection and a start value while predicate `p` is satisfied,
+      * going right to left.
+      *
+      * @param   z    the start value.
+      * @param   p    the predicate used to test if fold should continue.
+      * @param   op   the binary operator.
+      * @tparam  B    the result type of the binary operator.
+      * @return  the result of inserting `op` between consecutive elements of this collection,
+      *          going right to left with the start value `z` on the right.
+      */
     def foldRightWhile[B](z: B)(p: B => Boolean)(op: (A, B) => B): B =
       if (iterableLike.isEmpty) z
       else {
